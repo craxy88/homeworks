9396c6bb3b3229e80b982a44169bb427f24e5ae9
craxy88 <xxmarselxx7@gmail.com>
2023-07-18 19:54:28 +0400

    FIFTH COMMIT

--- Day 4/Homework/homework.py
+++ Day 4/Homework/homework.py
@@ -1,4 +1,4 @@
-age = input("Whats your age?: ")
-time_travel = input ("How many years would you like to time travel?: ")
-age_after_time_travel = (int(age) + int(time_travel))
-print("Age after time travel: " + str(age_after_time_travel))
+age = int(input("Whats your age? : "))
+time_travel = int(input("How many years would you like to time travel? : "))
+age_after_time_travel = age + time_travel
+print("Age after time travel =  " + str(age_after_time_travel))

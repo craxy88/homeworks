9a74f145d1119b75fbd271cae10aff6a46f21871
craxy88 <xxmarselxx7@gmail.com>
2023-07-21 22:09:07 +0400

    Sixth commit

---
+++ Day 5/Homework/homework.py
@@ -0,0 +1,8 @@
+name = input("Hello, whats your name? : ")
+surname = input("Please enter your surname : ")
+country_born = input("Where were you born? : ")
+age = int(input("Enter your age : "))
+height = input("Enter your height in centimetres : ")
+print("Your name is:" + name , surname + ". ", "You were born in:" + country_born + "." , "You are:" + str(age) + " Years Old." , "You are:" + height + "cm.")
+
+

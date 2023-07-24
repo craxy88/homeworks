4767776d45452fee0428502ec4485d6e3850ebca
craxy88 <xxmarselxx7@gmail.com>
2023-07-24 23:36:39 +0400

    Seventh commit

---
+++ Day 6/Classwork/classwork6.py
@@ -0,0 +1,63 @@
+# # Asks the user to enter the savings
+# savings = input("ramdeni dolari gaqvs?")
+
+# # Convert the user input into a float value and update the variable
+# savings = int(savings)
+
+# # Savings grow after 1 year at a 5% annual interest rate
+# balance = savings * 1.05 
+# print(balance)
+
+# print(11 + 5)
+# type(16.4)
+# distance = 14 
+# units = int("16")
+# print(distance + units)
+
+# print("14" + "16")
+
+# x = 5.6
+# print(type(x))
+# print(x)
+
+
+# age = input("Enter your age : ")
+# future = int(age) + 10
+# print("10 wlis shemdeg iqnebi " + str(future))
+
+# # print("10 wlis shemdeg iqnebi " + str(int(age) + 10 ) , "wlis") 
+
+# print(10 + 10)
+# p1 = 10
+# p2 = 10
+# print(p1 + p2)
+
+# alo1 = int(input())
+# alo2 = int(input())
+# print(alo1 + alo2)
+# alo = 15 < 5
+# print(alo)
+
+# print(type("16")) 
+
+# print(25 != 25)
+
+# age = 15 # INTEGER
+# saxeli = '20' # STRING
+# yiyliyo = 6.5 # FLOAT
+# arisBangeri = True # BOOL - BOOLEAN
+# print(arisBangeri)
+# 0 1 - orobiti - binary
+# 0 - False
+# 1 - True
+# print(25 != 25)
+
+
+# შედარების ოპერატორები
+# =  მინიჭების ოპერატორი
+# ==  უდრის
+# !=  არ უდრის
+#  მეტობა
+# <  ნაკლებობა
+# >=
+# <=
---
+++ Day 6/Homework/homework.py
@@ -0,0 +1,8 @@
+# 1. მომხმარებელს შეეკითხეთ ასაკი და დაპრინტეთ არის თუ არა 18< (True / False)
+# 2. მომხმარებელს შეეკითხეთ მისი და მამამისის ასაკი და დაპრინტეთ ჯამში 50< არის თუ არა 
+age = int(input("Enter your age : "))
+print(age < 18)     #დავალება 1. 
+
+son_age = int(input("Enter sons age : "))
+father_age = int(input("Enter fathers age : "))
+print(son_age + father_age < 50)      #დავალება 2.

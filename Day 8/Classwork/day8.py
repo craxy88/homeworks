fa4abc28dbbc555ecbb114bb15cd4e53d2725a3a
craxy88 <xxmarselxx7@gmail.com>
2023-08-05 16:23:09 +0400

    8th commit

---
+++ Day 8/Classwork/day8.py
@@ -0,0 +1,5 @@
+for i in range(2,20,2):
+    print("vano " +  str(i))
+
+for i in range (1,20,2):
+    print("Motiashvili " + str(i))
---
+++ Day 8/Homework/homework.py
@@ -0,0 +1,5 @@
+first = 10
+second = 10
+for i in range(1,first + 1): 
+    for o in range(1,second + 1):
+        print(f"{i} x {o}")
